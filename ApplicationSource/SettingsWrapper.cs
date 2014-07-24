1de4033c896600aaa0be59fe189f40df886e2022
Jonathan Brown <jbrown@control4.com>
2014-07-24 18:04:27 +0000

    Adding Automapper, DeliveryOrder with models, domain, and persistence

---
+++ ApplicationSource/SettingsWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using ApplicationSource.Interfaces;
+
+namespace ApplicationSource
+{
+    public class SettingsWrapper:ISettings
+    {
+        public string GetServerLocation {
+            get { return ConfigurationManager.AppSettings["ServerLocation"]; }
+        }
+    }
+}
