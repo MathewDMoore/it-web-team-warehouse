0e05b6d56a5452959e346a6e04673a1f02461d8b
Jonathan Brown <jbrown@control4.com>
2014-09-03 17:33:10 +0000

    WORKKKK!!!1

--- C4InventorySerialization/scripts/services/ScanOrderService.js
+++ C4InventorySerialization/scripts/services/ScanOrderService.js
@@ -2,7 +2,7 @@
     var scanOrderObject = {
         LookUp: function (orderId, isInternal) {
 
-            return $http({ data: { DeliveryNumber: orderId, IsInternal: isInternal }, method: "POST", url: "/ship/services/OrderDeliveryService.svc/OrderLookUp" }).success(function (response) {
+            return $http({ data: { DeliveryNumber: orderId, IsInternal: isInternal }, method: "POST", url: "/services/OrderDeliveryService.svc/OrderLookUp" }).success(function (response) {
                 return response;
             }).error(function(result) {
                 $log.error("ScanOrderService -> OrderLookUp " + result);
@@ -10,40 +10,40 @@
         },
         LookUpByMacId: function(macId) {
 
-            return $http({ data: macId, method: "POST", url: "/ship/services/OrderDeliveryService.svc/LocateMacIds" }).success(function(response) {
+            return $http({ data: macId, method: "POST", url: "/services/OrderDeliveryService.svc/LocateMacIds" }).success(function(response) {
                 return response;
             }).error(function(result) {
                 $log.error("ScanOrderService -> OrderLookUp " + result);
             });
         },
         SaveDeliveryItem: function(deliveryItem) {
-            return $http({ data: deliveryItem, method: "POST", url: "/ship/services/OrderDeliveryService.svc/SaveDeliveryItem" })
+            return $http({ data: deliveryItem, method: "POST", url: "/services/OrderDeliveryService.svc/SaveDeliveryItem" })
                 .success(function(result) {
                     return result;
                 });
         },
         ClearDelivery: function(delivery) {
             return $http({
-                data: delivery, method: "POST", url: "/ship/services/OrderDeliveryService.svc/ClearDelivery" })
+                data: delivery, method: "POST", url: "/services/OrderDeliveryService.svc/ClearDelivery" })
                 .success(function(result) {
                     return result;
                 });
         },
         ReturnDelivery: function(delivery) {
             return $http({
-                data: delivery, method: "POST", url: "/ship/services/OrderDeliveryService.svc/ReturnDelivery" })
+                data: delivery, method: "POST", url: "/services/OrderDeliveryService.svc/ReturnDelivery" })
                 .success(function(result) {
                     return result;
                 });
         },
         ReturnSelectedItems: function(ids,isInternal) {
-            return $http({ data: { Ids: ids, IsInternal: isInternal }, method: "POST", url: "/ship/services/OrderDeliveryService.svc/ReturnDeliveryLineItem" })
+            return $http({ data: { Ids: ids, IsInternal: isInternal }, method: "POST", url: "/services/OrderDeliveryService.svc/ReturnDeliveryLineItem" })
                 .success(function(result) {
                     return result;
                 });
         },
         VerifyDelivery: function(deliveryId) {
-            return $http({ data: deliveryId, method: "POST", url: "ship/services/OrderDeliveryService.svc/VerifyDelivery" })
+            return $http({ data: deliveryId, method: "POST", url: "/services/OrderDeliveryService.svc/VerifyDelivery" })
                 .success(function(result) {
                     return result;
                 });
