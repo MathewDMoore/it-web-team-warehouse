30753ab31a09e271abf6dfdb5a9c2d482d26eaf3
Jonathan Brown <jbrown@control4.com>
2014-11-19 21:32:49 +0000

    Fixed style sheet. Working on kit issue

--- JsUnitTests/Scripts/Tests/ScanOrderControllerTests.js
+++ JsUnitTests/Scripts/Tests/ScanOrderControllerTests.js
@@ -1,10 +1,11 @@
 ﻿/// <reference path="../libs/jasmine.js" />
 /// <reference path="../libs/jquery.min.js"/>
-/// <reference path="../libs/angular.min.js" />
 /// <reference path="../libs/angular-animate.min.js" />
 /// <reference path="../libs/angular-resource.min.js" />
 /// <reference path="../angular-sanitize.min.js" />
+
 /// <reference path="../../../c4inventoryserialization/scripts/angular.min.js" />
+/// <reference path="../libs/angular-mocks.js" />
 /// <reference path="../../../c4inventoryserialization/scripts/libs/angular-bootstrap/ui-bootstrap.min.js" />
 /// <reference path="../../../c4inventoryserialization/scripts/libs/angular.audio.js" />
 /// <reference path="../../../c4inventoryserialization/scripts/ng-table-export.js" />
@@ -16,28 +17,30 @@
 /// <reference path="../../../c4inventoryserialization/scripts/shippingapp.js" />
 /// <reference path="../../../c4inventoryserialization/scripts/services/firebasedeliveryservice.js" />
 /// <reference path="../../../c4inventoryserialization/scripts/services/scanorderservice.js" />
-/// <reference path="../../../c4inventoryserialization/scripts/controllers/scanordercontroller.js" />
+/// <reference path="../../../c4inventoryserialization/scripts/controllers/scanordercontroller2.js" />
 describe("Scan Order Controller Tests", function () {
-    var scope, $rootScope, _scanService, _ngTableParams, _ngAudio, _modal, _scan;
-    it("Test", function () {
-        expect(1).toBe(1);
-    });
+    var $scope, _scanService, _ngTableParams, _ngAudio, $modal;
     beforeEach(module("shipApp"));
-    //    $scope, $modal, $filter, $timeout, ngTableParams, ScanOrderService, FirebaseDeliveryService, ngAudio, CURRENTUSER
-    //    beforeEach(inject(function(_$rootScope_, $controller, ScanOrderService, _ngTableParams_,FirebaseDeliveryService, $timeout, $modal, _ngAudio_) {
-    //        scope = _$rootScope_.$new();
-    //        $rootScope = _$rootScope_;
-    //        _scanService = ScanOrderService;
-    //        _modal = $modal;
-    //        _ngAudio = _ngAudio_;
-    //        _ngTableParams = _ngTableParams_;
-    //        _scan = scope.scan;
-    //        _firebase = jasmine.createSpy();
-    //        $controller("ScanController as scan", {$scope:scope,FirebaseDeliveryService:_firebase, $filter:$filter,$timeout:$timeout,$modal:_modal,ngAudio:_ngAudio,ScanOrderService:_scanService, CURRENTUSER:'TEST'});
-    //    }));
+
+//    beforeEach(
+//        module(function ($provide) {
+//            $provide.constant("CURRENTUSER", "USER");
+//            $provide.constant("FIREBASE_URL", "URL");
+//        }));
+
+
+    beforeEach(inject(function (_$rootScope_, $provide, $controller, ScanOrderService, _ngTableParams_, FirebaseDeliveryService, $filter, $timeout, _$modal_, _ngAudio_) {
+        $scope = _$rootScope_.$new();
+        _scanService = ScanOrderService;
+        $modal = _$modal_;
+        _ngAudio = _ngAudio_;
+        _ngTableParams = _ngTableParams_;
+        $controller("ScanController as scan", { $scope: $scope, FirebaseDeliveryService: FirebaseDeliveryService, $filter: $filter, $timeout: $timeout, $modal: $modal, ngAudio: _ngAudio, ScanOrderService: _scanService, CURRENTUSER: "USER" });
+    }));
     describe("Default Properties", function () {
         it("Verify default values", function () {
-            expect(_scan.ShouldSelect).toBeFalsy();
+            expect($scope.scan.ShouldSelect).toBeFalsy();
+            //            expect(_scan.ShouldSelect).toBeFalsy();
         });
     });
 });
